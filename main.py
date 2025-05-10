023b3271d108819578647765c1592599b7e6e07e
XMAS <xmas@dhcp-10-8-038-100.mobile.reshsg.uci.edu>
2025-05-09 18:32:53 -0700

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def resource_path(relative_path):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.abspath(relative_path)
 
-save_path = "example_result.xlsx"
+save_path = "result.xlsx"
 
 def load_anp_emotion_dict(txt_path):
     """加载 ANP → 情绪得分字典的嵌套结构"""
@@ -136,4 +136,3 @@ for jpg in jpg_files:
 
     # save_to_excel(os.path.basename(jpg), anp_results, iqa_score, nima_mean, nima_std)
     save_to_excel(os.path.basename(jpg), anp_results, iqa_score, nima_mean, nima_std, max_emotions)
-
